d664482bacf019d73f3a7836a76cfa783b61d87d
Andreas Weinmann <andy.weinmann@gmail.com>
2020-10-28 20:01:43 +0100

    date changed to ISO8601 including year

--- core.lua
+++ core.lua
@@ -2527,7 +2527,7 @@ end
 
 function bepgp:getServerTime()
   local epoch = GetServerTime()
-  local d = date("%b-%d",epoch)
+  local d = date("%Y-%m-%d",epoch)
   local t = date("%H:%M:%S",epoch)
   local timestamp = string.format("%s %s",d,t)
   return epoch, timestamp
